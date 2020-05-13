f5a07ca3394b0ce8e2f435e5ffbed175f78f7e64
Tkachenko Artem <64965397+mishcha@users.noreply.github.com>
2020-05-14 01:18:41 +0300

    release v3

--- lab1/симм_крипта_1.py
+++ lab1/симм_крипта_1.py
@@ -1,7 +1,6 @@
 import math, pandas, re, copy
 
-#Открытие файла 
-with open("Something.txt", 'rb') as file: Text = file.read().decode('utf-8')
+with open("Jason Schreier.txt", 'rb') as file: Text = file.read().decode('utf-8')
 
 pattern_1 = r'ё'
 pattern_2 = r'ъ'    
@@ -9,36 +8,39 @@ pattern_3 = r'[^абвгдежзийклмнопрстуфхцчшщыьэюя ]
 
 Text = re.sub(pattern_1, 'е', Text.lower())
 Text = re.sub(pattern_2, 'ь', Text)
-Text = re.sub(pattern_3, '', Text)
+Text = re.sub(pattern_3, '', Text) 
 Text_with_space = Text
 Text = list(''.join(Text.split()))
 
-cyrillic_with_space = 'абвгдежзийклмнопрстуфхцчшщыьэюя '
-cyrillic = 'абвгдежзийклмнопрстуфхцчшщыьэюя'
+rus_with_space = 'абвгдежзийклмнопрстуфхцчшщыьэюя '
+rus_no_space = 'абвгдежзийклмнопрстуфхцчшщыьэюя'
 
-alphabet_no_space = dict.fromkeys(list(cyrillic), 0)
-alphabet = dict.fromkeys(list(cyrillic_with_space), 0)
+alphabet_no_space = dict.fromkeys(list(rus_no_space), 0)
+alphabet = dict.fromkeys(list(rus_with_space), 0) 
 
 
 #Подсчет букв в тексте
 for i in Text_with_space:alphabet[i] += 1
 for i in Text: alphabet_no_space[i] += 1
 
-def dictioanary_sorting(dictionary):
+def dictionary_sorting(dictionary):
     values = sum(dictionary.values())
-    frequency = [(i, round(dictionary[i]/values, 5)) for i in sorted(dictionary.keys(), key=dictionary.get, reverse=True)]
+    frequency = [(i, round(dictionary[i]/values, 7)) for i in sorted(dictionary.keys(), key=dictionary.get, reverse=True)]
     H_entropy = [(i, -dictionary[i]/values*math.log(dictionary[i] / values, 2)) for i in sorted(dictionary.keys(), key=dictionary.get, reverse=True)]
     
     return frequency, H_entropy
 
-print('Частота букв текста: ', dictioanary_sorting(alphabet)[0])
-print('Энтропия букв текста: ', dictioanary_sorting(alphabet)[1])
-print('Энтропия букв текста без пробелов: ', dictioanary_sorting(alphabet_no_space)[1])
+print('_______________________________________________________________________')
+print('Частота букв текста: ', dictionary_sorting(alphabet)[0])
+print('_______________________________________________________________________')
+print('Энтропия букв текста: ', dictionary_sorting(alphabet)[1])
+print('_______________________________________________________________________')
+print('Энтропия букв текста без пробелов: ', dictionary_sorting(alphabet_no_space)[1])
 
 
 #Инициализация датафрейма
-data_no_spaces = pandas.DataFrame(columns = list(cyrillic), index = list(cyrillic), data = 0.0)
-data_with_spaces = pandas.DataFrame(columns = list(cyrillic_with_space), index = list(cyrillic_with_space), data = 0.0)
+data_no_spaces = pandas.DataFrame(columns = list(rus_no_space), index = list(rus_no_space), data = 0.0)
+data_with_spaces = pandas.DataFrame(columns = list(rus_with_space), index = list(rus_with_space), data = 0.0)
 
 #Запись данных из словаря с биграммами в датафрейм
 def matrix(data, dict_):
@@ -79,13 +81,13 @@ def intersecting_bigrams(Text):
 
 
 #Запись пересекающихся и непересекающихся биграмм и их энтропиями текста с пробеламы в Excel
-temp_for_intersecting_bigrams = matrix(data_with_spaces, intersecting_bigrams(Text_with_space)) 
-temp_for_disjoint_bigrams = matrix(data_with_spaces, disjoint_bigrams(Text_with_space))
+array_for_intersecting_bigrams = matrix(data_with_spaces, intersecting_bigrams(Text_with_space)) 
+array_for_disjoint_bigrams = matrix(data_with_spaces, disjoint_bigrams(Text_with_space))
 
-temp_for_intersecting_bigrams[0].to_excel('Частоты пересекающихся биграмм с пробелами.xlsx')
-temp_for_disjoint_bigrams[0].to_excel('Частоты непересекающихся биграмм с пробелами.xlsx')
-temp_for_intersecting_bigrams[1].to_excel('Энтропия пересекающихся биграмм с пробелами.xlsx')
-temp_for_disjoint_bigrams[1].to_excel('Энтропия непересекающихся биграмм с пробелами.xlsx')
+array_for_intersecting_bigrams[0].to_excel('Частоты пересекающихся биграмм с пробелами.xlsx')
+array_for_disjoint_bigrams[0].to_excel('Частоты непересекающихся биграмм с пробелами.xlsx')
+array_for_intersecting_bigrams[1].to_excel('Энтропия пересекающихся биграмм с пробелами.xlsx')
+array_for_disjoint_bigrams[1].to_excel('Энтропия непересекающихся биграмм с пробелами.xlsx')
 
 #Запись пересекающихся и непересекающихся биграмм и их энтропиями в Excel
 matrix(data_no_spaces, intersecting_bigrams(Text))[1].to_excel('Энтропия пересекающихся биграмм без пробелов.xlsx')
